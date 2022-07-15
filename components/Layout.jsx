1846de45f70a802531628af7b2f4691469948d96
Maxim <hristivi8383@gmail.com>
2022-07-15 11:55:29 +0300

    metaTags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,9 @@ const Layout = ({children}) => {
     <div className='layout'>
       <Head>
         <title>Penis</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="description" content="Buy Rock Clothing and Accesorries" />
       </Head>
       <header>
         <Navbar />
